59ded3fec49f0da71cef013d935e8abafdad5923
nick balistreri <nick balistreri>
2016-02-12 14:56:49 -0800

    Tutorials L5

---
+++ app/Http/Controllers/CardsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Card;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class CardsController extends Controller
+{
+    public function index()
+    {
+        $cards = Card::all();
+
+        return view('cards.index', compact('cards'));
+    }
+
+    public function show(Card $card)
+    {
+       return view('cards.show', compact('card'));
+    }
+}
+
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+    @yield('header')
+</head>
+<body>
+
+    <div class="container">
+    @yield('content')
+    </div>
+
+    @yield('footer')
+
+</body>
+</html>
